15bbbc29256cf80359d9fa94771b25bf32aaab9f
rander <rander@localhost>
2017-10-10 00:02:29 +1100

    latexdiff between resubmitted version and r174.

--- diff.py
+++ diff.py
@@ -5,7 +5,7 @@ from os import path
 import re
 
 mathmode = 0
-old_rev = 86
+old_rev = 166
 
 new_tex = "dressed_faraday.tex"
 old_tex = "dressed_faraday@%i.tex" % old_rev
@@ -13,7 +13,7 @@ diff_out = "dressed_faraday_diff.tex"
 
 with open(diff_out, "w") as f:
     check_call(["latexdiff", "-V", "--flatten", "--math-markup=%i" % mathmode, "--mbox"
-                "--packages=amsmath,hyperref", "--exclude-textcmd=section,subsection",
+                "--packages=amsmath,hyperref", "--exclude-textcmd=section,subsection,bibliography",
                 "--exclude-safecmd=note,vect,Rb,abs,uvect,vls,thetaQWP,thetaN,reffig,refeq,partialD,totalD,epvec,HzWcm",
                 # "--exclude-safecmd=",
                 old_tex, new_tex], stdout=f, stderr=None)
